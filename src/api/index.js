5106eb98d4c51e0b3e102bf85ee75abed78a70c3
Mosaab Gamal <hello@mosaab.is-a.dev>
2025-12-18 06:16:47 +0200

    feat: create index file for API exports

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+export { default as axiosInstance } from './axiosInstance';
+export { authApi } from './authApi';
+export { studentApi } from './studentApi';
+export { courseApi } from './courseApi';
+export { courseCategoryApi } from './courseCategoryApi';
+export { enrollmentApi } from './enrollmentApi';
+export { instructorApi } from './instructorApi';
+export { scheduleApi } from './scheduleApi';
+export { userApi } from './userApi';
